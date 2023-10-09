0758ede6b268f3fb0c2e4f2600aef0d55d91fa4b
toptalentdevteam <ctori0816@gmail.com>
2023-10-09 13:18:09 +1100

    update

--- Assets/Scripts/GameEngine.cs
+++ Assets/Scripts/GameEngine.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class Level1 : MonoBehaviour
+public class GameEngine : MonoBehaviour
 {
     [SerializeField] private GameObject beforeStartDialog;
     [SerializeField] private GameObject endDialog;
+    [SerializeField] private GameObject failDialog;
     [SerializeField] private Sprite[] colors;
     [SerializeField] private Sprite defaultColor;
     [SerializeField] private Image[] colorImages;
@@ -15,7 +17,10 @@ public class Level1 : MonoBehaviour
     [SerializeField] private float startTime;
     [SerializeField] private float timeRemaining;
     [SerializeField] private int gameStatus;
+    [SerializeField] private int levelIndex;
+    [SerializeField] private int currentLevelIndex;
     [SerializeField] private Text lifeCountText;
+    [SerializeField] private Text currentCoinText;
     [SerializeField] private Text endResultText;
     [SerializeField] private Text endCoinText;
     [SerializeField] private Text endTimeText;
@@ -24,16 +29,24 @@ public class Level1 : MonoBehaviour
 
     private int colorIndex;
     private int panelIndex;
+    
+    private int coinNum;
     private int totalNum;
     private int correctNum;
     private int lifeNum;
     bool startCounter;
+    private bool isStarted;
     float levelTagTimer = 0.5f;
 
     void Start()
-    {
+    {        
         RandomColors();
         timeRemaining = startTime;
+        isStarted = false;
+
+        levelIndex = PlayerPrefs.GetInt("LEVEL_PASSED");
+        coinNum = PlayerPrefs.GetInt("COIN_OWN");
+        currentCoinText.text = coinNum.ToString();
     }
 
     void Update()
@@ -52,6 +65,7 @@ public class Level1 : MonoBehaviour
 
         if(gameStatus == 1)
         {
+            // Select color palette
             if(Input.GetMouseButtonDown(0))
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -62,11 +76,12 @@ public class Level1 : MonoBehaviour
                 {
                     if (hit.transform.gameObject.tag == "color")
                     {                        
-                        colorIndex = int.Parse(hit.transform.gameObject.name.Split(" ")[1]);
+                        colorIndex = int.Parse(hit.transform.gameObject.name.Split(" ")[1]);                   
                     }
                 }                
             }
 
+            // Select colors
             if (Input.GetMouseButtonUp(0))
             {
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -80,40 +95,60 @@ public class Level1 : MonoBehaviour
                         panelIndex = int.Parse(hit.transform.gameObject.name.Split(" ")[1]);
 
                         if (colorIndex == selectedColors[panelIndex])
-                        {
+                        {                            
                             colorImages[panelIndex].sprite = colors[colorIndex];
                             correctNum++;
 
-                            if(correctNum >= totalNum)
+                            // Pass level
+                            if (correctNum >= totalNum)
                             {
                                 gameStatus = 0;
                                 Time.timeScale = 0;
                                 endDialog.SetActive(true);
                                 endTimeText.text = countDownTimeText.text;
+                                
+                                // Compare with current level index
+                                if(levelIndex == currentLevelIndex)
+                                {
+                                    levelIndex++;
+                                    PlayerPrefs.SetInt("LEVEL_PASSED", levelIndex);
+                                }                                                                
 
-                                if(180f - timeRemaining <= 60f)
+                                // Players can get coins according to time
+                                if (180f - timeRemaining <= 60f)
                                 {
                                     endResultText.text = "Best";
                                     endCoinText.text = "3";
+                                    coinNum += 3;                                    
                                 }                            
                                 else if(timeRemaining <= 60f)
                                 {
                                     endResultText.text = "Good";
                                     endCoinText.text = "1";
+                                    coinNum += 1;
                                 }
                                 else
                                 {
                                     endResultText.text = "Better";
                                     endCoinText.text = "2";
+                                    coinNum += 2;
                                 }
+
+                                PlayerPrefs.SetInt("COIN_OWN", coinNum);
                             }
                         }
                         else
-                        {
+                        {                            
                             lifeNum--;
                             lifeCountText.text = lifeNum.ToString();
                             StopAllCoroutines();
                             StartCoroutine(DelayToShowAlert("Wrong!"));
+
+                            //Fail level
+                            if(lifeNum == 0)
+                            {
+                                failDialog.SetActive(true);
+                            }
                         }
                     }
                 }                
@@ -124,7 +159,107 @@ public class Level1 : MonoBehaviour
     // Arrange colors
     public void RandomColors()
     {
-        for(int i = 0; i < 2; i++)
+        switch (currentLevelIndex)
+        {
+            case 1:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 2:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 3:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 4:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 5:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 6:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 7:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 8:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 9:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 10:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 11:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 12:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 13:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 14:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 15:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+            case 16:
+                {
+                    totalNum = 2;
+                    lifeNum = 4;
+                    break;
+                }
+        }
+
+        for (int i = 0; i < totalNum; i++)
         {
             selectedColors[i] = (System.DateTime.Now.Millisecond * UnityEngine.Random.Range(5, 15)) % 8;
             colorImages[i].sprite = colors[selectedColors[i]];           
@@ -134,26 +269,44 @@ public class Level1 : MonoBehaviour
         {
             selectedColors[1] = (selectedColors[1] + 1) % 8;
             colorImages[1].sprite = colors[selectedColors[1]];
-        }
-
-        totalNum = 2;
-        lifeNum = 4;
+        }        
     }
 
     // Start game
     public void StartGame()
-    { 
-        startTime += 183f;
-        timeRemaining = startTime;
-
-        for(int i = 0; i < colorImages.Length; i++)
+    {
+        if (!isStarted)
         {
-            colorImages[i].sprite = defaultColor;
-        }
+            isStarted = true;
+            startTime += 8f;
+            timeRemaining = startTime;
 
-        StartCoroutine(DelayToStartGame());
+            for (int i = 0; i < colorImages.Length; i++)
+            {
+                colorImages[i].sprite = defaultColor;
+            }
+
+            StartCoroutine(DelayToStartGame());     
+        }               
     }
 
+    // Go to next level
+    public void NextLevel()
+    {
+        SceneManager.LoadScene("Level " + (currentLevelIndex + 1));
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("Level " + currentLevelIndex);
+    }
+
+    public void BackBtnClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    // Delay for 3 secs before starting game
     IEnumerator DelayToStartGame()
     {
         beforeStartDialog.SetActive(true);
@@ -178,12 +331,15 @@ public class Level1 : MonoBehaviour
         if (timeRemaining <= 0)
         {
             if(gameStatus == 0)
-            {  
-                StartGame();
+            {
+                StartGame(); 
             }
             else
             {
-
+                // End game
+                gameStatus = 0;        
+                Time.timeScale = 0;
+                failDialog.SetActive(true);            
             }                        
         }
 
@@ -200,6 +356,7 @@ public class Level1 : MonoBehaviour
         ShowTime();
     }
 
+    // Display timer
     void ShowTime()
     {
         int minutes;
--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
-    
+    [SerializeField] private Button[] levelBtns;
 
     void Start()
     {
+        if(PlayerPrefs.GetInt("LEVEL_PASSED") == 0)
+        {
+            PlayerPrefs.SetInt("LEVEL_PASSED", 1);
+        }
         
+        for(int i = 0; i < PlayerPrefs.GetInt("LEVEL_PASSED"); i++)
+        {
+            levelBtns[i].interactable = true;
+        }
     }
 
     void Update()
